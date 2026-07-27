8ab7bc75694c7253a21b2002b65e5778e960718c
Ratwaredev <131556151+Ratwaredev@users.noreply.github.com>
2026-07-27 14:33:50 -0300

    route assistant and admin surfaces

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AssistantApp from './AssistantApp';
+import AdminApp from './AdminApp';
 import './assistant.css';
+import './admin.css';
+
+const isAdminView = new URLSearchParams(window.location.search).get('view') === 'admin';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AssistantApp />
+    {isAdminView ? <AdminApp /> : <AssistantApp />}
   </React.StrictMode>
 );
